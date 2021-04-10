9b410008b2e93ff93f8fbddf6213a299a8cf0748
mustafabukulmez <mustafabukulmez@hotmail.com.tr>
2021-04-10 14:41:41 +0300

    C# Operator Overloaded İşlemi

---
+++ opertor_overloaded_app/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace opertor_overloaded_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Kasiyer kasiyer_Mustafa = new Kasiyer("Mustafa", 500);
+            Kasiyer kasiyer_Ahmet = new Kasiyer("Ahmet", 800);
+            Kasiyer kasiyer_Ayşe = new Kasiyer("Ayşe", 1200);
+            MessageBox.Show(kasiyer_Mustafa.ToString());
+            //MessageBox.Show(kasiyer_Ahmet);
+            //MessageBox.Show(kasiyer_Ayşe);
+
+            Kasiyer ksyr_toplam_Satis = kasiyer_Mustafa + kasiyer_Ahmet + kasiyer_Ayşe;
+            MessageBox.Show(ksyr_toplam_Satis.ToString());
+
+        }
+    }
+}
---
+++ opertor_overloaded_app/Kasiyer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace opertor_overloaded_app
+{
+    class Kasiyer
+    {
+        int _satis_adet;
+        string _calisan_adi;
+
+        public Kasiyer(string calisan_adi, int satis_adet)
+        {
+            _calisan_adi = calisan_adi;
+            _satis_adet = satis_adet;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} Adet Satış", _calisan_adi, _satis_adet);
+        }
+
+
+        public static Kasiyer operator +(Kasiyer Kasiyer1, Kasiyer Kasiyer2)
+        {
+            int toplam_Satis = Kasiyer1._satis_adet + Kasiyer2._satis_adet;
+            return new Kasiyer("Toplam", toplam_Satis);
+        }
+
+        public static Kasiyer operator -(Kasiyer Kasiyer1, Kasiyer Kasiyer2)
+        {
+            int toplam_Satis = Kasiyer1._satis_adet - Kasiyer2._satis_adet;
+            return new Kasiyer("Toplam", toplam_Satis);
+        }
+        public static Kasiyer operator *(Kasiyer Kasiyer1, Kasiyer Kasiyer2)
+        {
+            int toplam_Satis = Kasiyer1._satis_adet - Kasiyer2._satis_adet;
+            return new Kasiyer("Toplam", toplam_Satis);
+        }
+        public static Kasiyer operator /(Kasiyer Kasiyer1, Kasiyer Kasiyer2)
+        {
+            int toplam_Satis = Kasiyer1._satis_adet - Kasiyer2._satis_adet;
+            return new Kasiyer("Toplam", toplam_Satis);
+        }
+    }
+}
